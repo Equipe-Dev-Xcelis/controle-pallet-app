56aa3024c636c1450bc501a76c20e9be30cb4513
GabsLima121 <gabriel.lima121@outlook.com>
2021-04-21 01:10:31 -0300

    falta apenas 2 paginas para a finalizacao

--- js/saldo.js
+++ js/saldo.js
@@ -1,29 +1,56 @@
-const contagemPallets = document.getElementById("contagemPallets");
-const saldoBtn = document.getElementById("saldoBtn");
+var gip
+var quantidade_pallets_expedidos
 
-const database2 = firebase.firestore();
-const collection2 = database2.collection('saldoPallets');
+function validarGip() {
+    gip = parseInt($('#gip').val())
 
+    const isGipInvalid = !gip
+    if (isGipInvalid) {
+        $('#gip').css({ boxShadow: '0 0 5px red' })
 
-saldoBtn.addEventListener('click', e => {
-    e.preventDefault();
-    collection2.doc(contagemPallets.value).set({
-        contagemPallets_: contagemPallets.value
-    })
-    .then(() => {alert('Dados enviados');})
-    .catch(error => {console.error(error)});
-});
+        return false
+    }
 
-lerSaldo.addEventListener('click', e => {
-    e.preventDefault();
-    collection2.get()
-    .then(snapshot => {
-        snapshot.forEach(contagemPallets => {
-            console.log(contagemPallets.id, ' => ', contagemPallets.data());
-        });
-        alert("Dados carregados com sucesso! Aperte F12 para visualizar!")
-    })
-    .catch(error => {
-        console.log(error);
-    });
-});
+    $('#gip').css({ boxShadow: 'none' })
+
+    return true
+}
+
+function validarquantidade_pallets_expedidos() {
+    quantidade_pallets_expedidos = $('#quantidade_pallets_expedidos').val()
+
+    const isquantidade_pallets_expedidosInvalid = !quantidade_pallets_expedidos
+    if (isquantidade_pallets_expedidosInvalid) {
+        $('#quantidade_pallets_expedidos').css({ boxShadow: '0 0 5px red' })
+        return false
+    }
+
+    $('#quantidade_pallets_expedidos').css({ boxShadow: 'none' })
+
+    return true
+}
+
+$('#search-gip').on('submit', function (e) {
+    e.preventDefault()
+
+    const gipValida = validarGip();
+
+    if (gipValida) {
+        var token = localStorage.getItem('token')
+
+        var config = {
+            headers: {
+                Authorization: 'Bearer ' + token
+            }
+        }
+        //quantidade_pallets_expedidos
+
+        api.get(`/gips/${gip}`, config)
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+})
