b62bbaa68e0c46143361a905635fefd2ec310d07
GabsLima121 <gabriel.lima121@outlook.com>
2021-07-22 15:43:21 -0300

    msg add

--- js/createShipping.js
+++ js/createShipping.js
@@ -1,7 +1,8 @@
 $('#cadastro-transportadora').on('submit', function (e) {
     e.preventDefault();
 
-    var nome = $('#transportadora').val()
+    var transportadora = $('#transportadora').val()
+    var nome = transportadora.toUpperCase()
 
     var shippingData = {
         nome
@@ -9,7 +10,12 @@ $('#cadastro-transportadora').on('submit', function (e) {
 
     api.post('/transportadora', shippingData)
         .then(function (response) {
-            console.log(response.data);
+            $('#alerta').append(`
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            ${response.data.message}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        `)
         }).catch(function (error) {
             console.log(error);
         })
